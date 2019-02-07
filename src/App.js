54925d54f3f12b1c7d7a1136a818c80493faab99
Deniz Tetik <deniztetik31@gmail.com>
2019-02-08 02:15:29 +0800

    First commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,38 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+import usePromise from "@aslan-hooks/use-promise";
+
+const App = () => {
+  const fetchPokemon = async () => {
+    const result = await fetch("https://pokeapi.co/api/v2/pokemon");
+    return result.json();
+  };
+
+  const [loading, result] = usePromise(fetchPokemon);
+
+  console.log("loading", loading);
+  console.log("result", result);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+};
 
 export default App;
