80b7b0ae047dc5e5bc4bed3306912ddf290055e0
Deniz Tetik <deniztetik31@gmail.com>
2019-02-08 04:16:43 +0800

    Separate Pokemon component and load images and number in each one

--- src/App.js
+++ src/App.js
@@ -5,16 +5,31 @@ import styled from "styled-components";
 
 import useFetch from "@aslan-hooks/use-fetch";
 
+import Pokemon from "./Pokemon";
+
+const PokemonWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+
 const App = () => {
-  const [loading, result] = useFetch("https://pokeapi.co/api/v2/pokemon");
+  const [loading, result] = useFetch(
+    "https://pokeapi.co/api/v2/pokemon?limit=151",
+  );
 
   return (
-    <ul>
-      {result &&
-        result.results.map(pokemon => (
-          <li key={pokemon.name}>{pokemon.name}</li>
-        ))}
-    </ul>
+    <>
+      {loading ? (
+        <div>Loading</div>
+      ) : (
+        <PokemonWrapper>
+          {result &&
+            result.results.map(pokemon => (
+              <Pokemon key={pokemon.name} name={pokemon.name} />
+            ))}
+        </PokemonWrapper>
+      )}
+    </>
   );
 };
 
---
+++ src/Pokemon.js
@@ -0,0 +1,48 @@
+import React from "react";
+// import { useState, useEffect, useRef } from "react";
+
+import Loader from "react-loader-spinner";
+
+// import styled from "styled-components";
+import "styled-components/macro";
+
+import capitalize from "lodash/capitalize";
+
+import useFetch from "@aslan-hooks/use-fetch";
+
+// const useIntersection = options => {
+//   const [observerEntry, setEntry] = useState({});
+//   const elRef = useRef();
+
+//   useEffect(() => {
+//     const observer = new IntersectionObserver(
+//       entries => setEntry(entries[0]),
+//       options,
+//     );
+//     observer.observe(elRef.current);
+//     return () => observer.disconnect();
+//   }, [elRef]);
+//   return { observerEntry, elRef };
+// };
+
+const Pokemon = ({ name }) => {
+  const [loading, pokemonInfo] = useFetch(
+    `https://pokeapi.co/api/v2/pokemon/${name}/`,
+  );
+
+  const imgSrc = pokemonInfo ? pokemonInfo.sprites.front_default : null;
+  const pokeNum = pokemonInfo ? `#${pokemonInfo.id}` : "";
+
+  return (
+    <div css="margin: 20px">
+      {loading ? (
+        <Loader type="Grid" color="grey" height={125} width={125} />
+      ) : (
+        <img src={imgSrc} alt={name} width={125} />
+      )}
+      <div css="text-align: center">{`${pokeNum} ${capitalize(name)}`}</div>
+    </div>
+  );
+};
+
+export default Pokemon;
