5ace7961db2b539a026a6381f1a73becde0c1788
Deniz Tetik <deniztetik31@gmail.com>
2019-02-17 18:25:37 +0800

    further isolate effect, with `useFetchPokemon` in its own file

--- src/App.js
+++ src/App.js
@@ -3,10 +3,9 @@ import "./App.css";
 
 import styled from "styled-components";
 
-import useLocalStorage from "@aslan-hooks/use-local-storage";
-
 import Pokemon from "./Pokemon";
-import useFetch from "@aslan-hooks/use-fetch";
+
+import useFetchPokemon from "./effects/useFetchPokemon";
 
 const PokemonWrapper = styled.div`
   display: flex;
@@ -15,50 +14,10 @@ const PokemonWrapper = styled.div`
   justify-content: center;
 `;
 
-const useFetchPokemon = (latestPokemonToFetch, setLoading, setResult) => {
-  const getPokemonFromLocalStorage = () =>
-    JSON.parse(window.localStorage.getItem("pokemon"));
-
-  const setPokemonInLocalStorage = pokemon =>
-    window.localStorage.setItem("pokemon", JSON.stringify(pokemon));
-
-  const fetchPokemonFromAPI = async limit => {
-    const res = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}`);
-    return await res.json();
-  };
-
-  const fetchPokemon = async () => {
-    let res;
-    setLoading(true);
-
-    const pokemonFromLocalStorage = getPokemonFromLocalStorage();
-    const latestPokemonInLocalStorage =
-      pokemonFromLocalStorage &&
-      pokemonFromLocalStorage.results.length >= latestPokemonToFetch;
-
-    if (latestPokemonInLocalStorage) {
-      res = pokemonFromLocalStorage;
-    } else {
-      res = await fetchPokemonFromAPI(latestPokemonToFetch);
-      setPokemonInLocalStorage(res);
-    }
-
-    setResult(res);
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    fetchPokemon(latestPokemonToFetch, setLoading, setResult);
-  }, [latestPokemonToFetch]);
-};
-
 const App = () => {
-  const [loading, setLoading] = useState(true);
-  const [result, setResult] = useState(null);
-
   const [latestPokemonToFetch, setLatestPokemonToFetch] = useState(151);
 
-  useFetchPokemon(latestPokemonToFetch, setLoading, setResult);
+  const [loading, result] = useFetchPokemon(latestPokemonToFetch);
 
   return (
     <>
---
+++ src/effects/useFetchPokemon.js
@@ -0,0 +1,34 @@
+import useLocalStorage from "@aslan-hooks/use-local-storage";
+import usePromise from "@aslan-hooks/use-promise";
+
+const useFetchPokemon = latestPokemonToFetch => {
+  const [pokemonFromLocalStorage, setPokemonInLocalStorage] = useLocalStorage(
+    "pokemon",
+  );
+
+  const fetchPokemonFromAPI = async limit => {
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}`);
+    return await res.json();
+  };
+
+  const fetchPokemon = async () => {
+    const latestPokemonInLocalStorage =
+      pokemonFromLocalStorage &&
+      pokemonFromLocalStorage.results.length >= latestPokemonToFetch;
+
+    if (latestPokemonInLocalStorage) {
+      return pokemonFromLocalStorage;
+    } else {
+      const res = await fetchPokemonFromAPI(latestPokemonToFetch);
+      //   TODO: put somewhere else... need to better isolate local storage...
+      setPokemonInLocalStorage(res);
+      return res;
+    }
+  };
+
+  const [loading, result] = usePromise(fetchPokemon, [latestPokemonToFetch]);
+
+  return [loading, result];
+};
+
+export default useFetchPokemon;
