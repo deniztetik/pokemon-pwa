f955ce06b9a4ca242fe0a9bc79ad4a5b93488c9b
Deniz Tetik <deniztetik31@gmail.com>
2019-02-18 19:23:49 +0800

    cleanup

--- src/Pokemon.js
+++ src/Pokemon.js
@@ -1,29 +1,12 @@
 import React from "react";
-// import { useState, useEffect, useRef } from "react";
 
 import Loader from "react-loader-spinner";
 
-// import styled from "styled-components";
 import "styled-components/macro";
 
 import capitalize from "lodash/capitalize";
 
 import useFetchPokemon from "./effects/useFetchPokemon";
-
-// const useIntersection = options => {
-//   const [observerEntry, setEntry] = useState({});
-//   const elRef = useRef();
-
-//   useEffect(() => {
-//     const observer = new IntersectionObserver(
-//       entries => setEntry(entries[0]),
-//       options,
-//     );
-//     observer.observe(elRef.current);
-//     return () => observer.disconnect();
-//   }, [elRef]);
-//   return { observerEntry, elRef };
-// };
 
 const Pokemon = ({ name }) => {
   const [loading, pokemonInfo] = useFetchPokemon(name);
